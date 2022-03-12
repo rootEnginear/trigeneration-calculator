90953ca290a95c98d7eca5b66d1e71162c883572
rootEnginear <suthep.chanchuphol@outlook.com>
2022-03-12 23:45:10 +0700

    feat: add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
 
+const theme = extendTheme({
+	fonts: {
+		heading: 'Kanit, sans-serif',
+		body: 'Sarabun, sans-serif',
+	},
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<ChakraProvider theme={theme}>
+			<Component {...pageProps} />
+		</ChakraProvider>
+	)
 }
 
 export default MyApp
