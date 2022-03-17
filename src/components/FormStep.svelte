b3a4faa5b3c28fd496be34c9e0a872db112ea418
rootEnginear <suthep.chanchuphol@outlook.com>
2022-03-18 01:40:49 +0700

    feat: go top every step

--- src/components/FormStep.svelte
+++ src/components/FormStep.svelte
@@ -3,12 +3,22 @@
 
 	$: canPrev = currentStep > 0;
 	$: canNext = currentStep < 4;
+
+	const goTop = () => {
+		window.scrollTo(0, 0);
+	};
 </script>
 
 <div class="columns is-mobile">
 	{#if canPrev}
 		<div class="column is-narrow">
-			<button class="button" on:click={() => currentStep--}>
+			<button
+				class="button"
+				on:click={() => {
+					currentStep--;
+					goTop();
+				}}
+			>
 				<span class="icon is-small">
 					<i class="fa fa-arrow-left" />
 				</span>
@@ -19,7 +29,13 @@
 	<div class="column" />
 	{#if canNext}
 		<div class="column is-narrow">
-			<button class="button" on:click={() => currentStep++}>
+			<button
+				class="button"
+				on:click={() => {
+					currentStep++;
+					goTop();
+				}}
+			>
 				<span>ถัดไป</span>
 				<span class="icon is-small">
 					<i class="fa fa-arrow-right" />
